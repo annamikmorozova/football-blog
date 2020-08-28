ab64dc53d37a9972fee9742f3f9041f4ea081b56
Anna Morozova <anna.mik.morozova@gmail.com>
2020-08-28 00:24:05 -0400

    Feat: library component

--- client/components/About.js
+++ client/components/About.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-// import {Link} from "react-router-dom";
 import {Col} from "reactstrap";
 
 export default class About extends Component {
---
+++ client/components/Library.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+// import {Link} from "react-router-dom";
+import {Col} from "reactstrap";
+
+class Library extends Component {
+	render() {
+		return (
+			<div>
+				<h1 className="library-title">Library</h1>
+				<div>images to be here</div>
+			</div>
+		);
+	}
+}
+
+export default Library;
