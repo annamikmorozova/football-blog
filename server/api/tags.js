52909533b50d042442bf5636204277d15b35539e
Anna Morozova <anna.mik.morozova@gmail.com>
2020-09-12 21:57:23 -0400

    Feat: get tags route

---
+++ server/api/tags.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const {Tag} = require("../db/models");
+
+router.get("/", async (req, res, next) => {
+	try {
+		const tags = await Tag.findAll();
+		res.json(tags);
+	} catch (error) {
+		next(error);
+	}
+});
