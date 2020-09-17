892c3edf8c5d29b5d5dd2094cc1ac04757208f83
Anna Morozova <anna.mik.morozova@gmail.com>
2020-09-17 19:36:39 -0400

    Style: new navbar position

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -13,55 +13,55 @@ class MainNavbar extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<div className="navbar-container">
 				<img src="/header.png" alt="header" className="header-image" />
-				<Navbar className="navbar-style" expand="lg">
-					<Col className="navbar-title-and-menu">
-						{this.props.isLoggedIn ? (
-							<div>
-								<h1 className="website-name">FOOTBALL76</h1>
-								<Row className="navbar-row">
-									<Link className="nav-admin-color" to="/home">
-										Admin
-									</Link>
-									<Link className="nav-admin-color" to="/about">
-										About
-									</Link>
-									<Link className="nav-admin-color" to="/posts">
-										Posts
-									</Link>
-									<Link className="nav-admin-color" to="/gallery">
-										Gallery
-									</Link>
-									<Link className="posts-navbar-link" to="/posts">
-										{" "}
-										<IoIosFootball color="white" size={38} />
-									</Link>
-									<a
-										className="nav-admin-color"
-										href="/posts"
-										onClick={this.props.handleClick}
-									>
-										Logout
-									</a>
-								</Row>
-							</div>
-						) : (
-							<div className="navbar-text">
-								<h1 className="website-name">FOOTBALL76</h1>
-								<Row className="navbar-row">
-									<Link to="/about">About</Link>
-									<Link to="/posts">Posts</Link>
-									<Link to="/gallery">Library</Link>
-									<Link className="posts-navbar-link" to="/posts">
-										{" "}
-										<IoIosFootball color="white" size={38} />
-									</Link>
-								</Row>
-							</div>
-						)}
-					</Col>
-				</Navbar>
+				{/* <Navbar className="navbar-style" expand="lg"> */}
+				<Col className="navbar-title-and-menu">
+					{this.props.isLoggedIn ? (
+						<div>
+							<h1 className="website-name">FOOTBALL76</h1>
+							<Row className="navbar-row">
+								<Link className="nav-admin-color" to="/home">
+									Admin
+								</Link>
+								<Link className="nav-admin-color" to="/about">
+									About
+								</Link>
+								<Link className="nav-admin-color" to="/posts">
+									Posts
+								</Link>
+								<Link className="nav-admin-color" to="/gallery">
+									Gallery
+								</Link>
+								<Link className="posts-navbar-link" to="/posts">
+									{" "}
+									<IoIosFootball color="white" size={38} />
+								</Link>
+								<a
+									className="nav-admin-color"
+									href="/posts"
+									onClick={this.props.handleClick}
+								>
+									Logout
+								</a>
+							</Row>
+						</div>
+					) : (
+						<div className="navbar-text">
+							<h1 className="website-name">FOOTBALL76</h1>
+							<Row className="navbar-row">
+								<Link to="/about">About</Link>
+								<Link to="/posts">Posts</Link>
+								<Link to="/gallery">Gallery</Link>
+								<Link className="posts-navbar-link" to="/posts">
+									{" "}
+									<IoIosFootball color="white" size={38} />
+								</Link>
+							</Row>
+						</div>
+					)}
+				</Col>
+				{/* </Navbar> */}
 			</div>
 		);
 	}
