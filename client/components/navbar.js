7ccc3d9e04f2ce67d516478697dad6535f083122
Anna Morozova <anna.mik.morozova@gmail.com>
2020-09-17 00:03:15 -0400

    Feat: add new header

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,6 +14,7 @@ class MainNavbar extends React.Component {
 	render() {
 		return (
 			<div>
+				<img src="/header.png" alt="header" className="header-image" />
 				<Navbar className="navbar-style" expand="lg">
 					<Col className="navbar-title-and-menu">
 						{this.props.isLoggedIn ? (
