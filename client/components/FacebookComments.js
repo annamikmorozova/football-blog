f4baf697dd6b09c1969ac5fd0628757af1be516a
Anna Morozova <anna.mik.morozova@gmail.com>
2020-10-15 17:46:21 -0400

    Fix: facebook api re-rendering

--- client/components/FacebookComments.js
+++ client/components/FacebookComments.js
@@ -2,6 +2,30 @@ import React, {Component} from "react";
 import {FacebookProvider, Comments} from "react-facebook";
 
 export default class FacebookComments extends Component {
+	componentDidMount() {
+		FB.XFBML.parse();
+
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId: process.env.FB_APPID,
+				cookie: true, // enable cookies to allow the server to access the session
+				xfbml: true, // parse social plugins on this page
+				version: "v2.5" // use version 2.1
+			});
+		};
+
+		// Load the SDK asynchronously
+		(function(d, s, id) {
+			var js,
+				fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		})(document, "script", "facebook-jssdk");
+	}
+
 	render() {
 		if (this.props.id) {
 			return (
